1b09ee1204c704dd68835504ab353237eccc1e62
phani522 <phanikanthjonnalagadda663@gmail.com>
2023-12-15 11:48:57 -0600

    Using logger

--- src/main/java/com/demo/SpringDockerJenkinsSampleApplication.java
+++ src/main/java/com/demo/SpringDockerJenkinsSampleApplication.java
@@ -8,9 +8,18 @@ import ch.qos.logback.classic.Logger;
 
 @SpringBootApplication
 public class SpringDockerJenkinsSampleApplication {
+	static Logger logger = (Logger) LoggerFactory.getLogger(SpringDockerJenkinsSampleApplication.class);
 
 	public static void main(String[] args) {
 	    
+		try {
+			logger.info("Logger used for output");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
 		SpringApplication.run(SpringDockerJenkinsSampleApplication.class, args);
 		System.out.println("Jenkins integrated with Git, docker");
 		
