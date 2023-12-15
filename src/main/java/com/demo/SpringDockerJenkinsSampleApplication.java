682c92b2ef9b465ef6390fd751e0003d7d91858d
phani522 <phanikanthjonnalagadda663@gmail.com>
2023-12-15 11:40:30 -0600

    updated main() method

--- src/main/java/com/demo/SpringDockerJenkinsSampleApplication.java
+++ src/main/java/com/demo/SpringDockerJenkinsSampleApplication.java
@@ -1,15 +1,19 @@
 package com.demo;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Logger;
 
 @SpringBootApplication
 public class SpringDockerJenkinsSampleApplication {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Jenkins integrated with Git, docker");
+	    
 		SpringApplication.run(SpringDockerJenkinsSampleApplication.class, args);
+		System.out.println("Jenkins integrated with Git, docker");
+		
 	}
 
 }
